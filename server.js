b9815e42390c807834c413994c9df04fecf61cc1
Melanie Elizabeth <melanietello2020@gmail.com>
2026-09-23 20:57:12 -0500

    feat: agregar ruta GET para obtener videojuegos

--- server.js
+++ server.js
@@ -19,6 +19,25 @@ function sendError(result, error){
     });
 };
 
+app.get('/videojuegos', async (require, result) => {
+    try{
+        const [res] = await db.query(
+            `SELECT id, titulo, genero, desarrollador, fecha_lanz, precio, descripcion
+            FROM videojuegos
+            ORDER BY id DESC
+            LIMIT 20`
+        );
+
+        if(res.length == 0){
+            throw new Error('No hay registros');
+        }
+
+        sendSuccess(result, res);
+    }catch(error){
+        sendError(result, error);
+    }
+});
+
 app.delete('/videojuegos/:id', async (require, result) => {
     try{
         const { id } = require.params;
