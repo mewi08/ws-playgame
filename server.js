6e6d52726f20ecaaaf2f08f98fd942679b4db08d
Melanie Elizabeth <melanietello2020@gmail.com>
2026-09-23 19:48:09 -0500

    chore: agregar funciones sendSuccess y sendError para manejo de respuestas

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const app = express();
 
 app.use(express.json());
 
+function sendSuccess(result, datos){
+    result.status(200).send({
+        success: true,
+        datos,
+    });
+};
+
+function sendError(result, error){
+    result.status(error.status || 500).send({
+        success: false,
+        mensaje: error.message,
+    });
+};
+
 db.getConnection((err) => {
     if(err) throw err;
     console.log('Conectado a la base de datos playgame');
